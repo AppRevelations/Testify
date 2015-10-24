4f8a07fc763d7f67ab3ffb5aed23abef8b8deeb1
Lakshay Chhikara <lakshay.chhikara09@gmail.com>
2015-10-24 23:12:27 +0530

    GridLayout asynctask bug fix

--- app/src/main/java/com/apprevelations/testify/MainActivity.java
+++ app/src/main/java/com/apprevelations/testify/MainActivity.java
@@ -5,13 +5,16 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,25 +22,30 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        mRecyclerView = (RecyclerView) findViewById(R.id.video_recycler);
+
         Toolbar toolBar=(Toolbar) findViewById(R.id.toolbar_select);
         toolBar.setTitle("Select Video ");
         setSupportActionBar(toolBar);
 
         Uri uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
         String[] projection = { MediaStore.Video.VideoColumns.DATA };
-        Cursor c = getContentResolver().query(uri, projection, null, null, null);
-        int vidsCount = 0;
-        if (c != null) {
-            vidsCount = c.getCount();
-            while (c.moveToNext()) {
-                Log.d("VIDEO", c.getString(0));
+        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
 
-              //  Bitmap thumb = ThumbnailUtils.createVideoThumbnail(c.getString(0),
-               //         MediaStore.Images.Thumbnails.MINI_KIND);
+        String paths[] = new String[cursor.getCount()];
 
+        if (cursor != null) {
+            int index = 0;
+            while (cursor.moveToNext()) {
+                paths[index++] = cursor.getString(0);
             }
-            c.close();
+            cursor.close();
         }
+
+        StaggeredGridLayoutManager mGridLayoutManager = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(mGridLayoutManager);
+        VideoAdapter mVideoAdapter = new VideoAdapter(paths);
+        mRecyclerView.setAdapter(mVideoAdapter);
     }
 
     @Override
--- app/src/main/java/com/apprevelations/testify/VideoAdapter.java
+++ app/src/main/java/com/apprevelations/testify/VideoAdapter.java
@@ -1,28 +1,111 @@
 package com.apprevelations.testify;
 
-import android.content.Context;
-import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
+import android.os.AsyncTask;
+import android.provider.MediaStore;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
-public class VideoAdapter extends CursorAdapter{
+import java.util.HashMap;
 
+public class VideoAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    public VideoAdapter(Context context, Cursor c, int flags) {
-        super(context, c, flags);
+    String[] mVideos;
+    HashMap<RecyclerView.ViewHolder, LoadThumbnail> mHashMap;
+
+    public VideoAdapter(String[] videos) {
+        mVideos = videos;
+        mHashMap = new HashMap<>();
     }
-
-
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recycler_singleview, parent, false);
 
+        DataViewHolder dataViewHolder = new DataViewHolder(view);
+        return dataViewHolder;
     }
 
     @Override
-    public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return null;
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
+        ((DataViewHolder) viewHolder).thumbnail.setImageBitmap(null);
+
+        LoadThumbnail mLoadThumbnail = new LoadThumbnail(mVideos, position, viewHolder);
+
+        if (mHashMap.containsKey(viewHolder)) {
+            (mHashMap.get(viewHolder)).cancel(true);
+        }
+
+        mHashMap.put(viewHolder, mLoadThumbnail);
+        mLoadThumbnail.execute();
     }
 
+    @Override
+    public int getItemCount() {
+        return mVideos.length;
+    }
+
+    @Override
+    public void onViewRecycled(RecyclerView.ViewHolder holder) {
+        (mHashMap.get(holder)).cancel(true);
+    }
+
+    @Override
+    public void onDetachedFromRecyclerView(RecyclerView recyclerView) {
+        super.onDetachedFromRecyclerView(recyclerView);
+    }
+
+    public static class DataViewHolder extends RecyclerView.ViewHolder {
+
+        protected ImageView thumbnail;
+        protected LinearLayout selected;
+        protected ProgressBar progressBar;
+
+        public DataViewHolder(View itemView) {
+            super(itemView);
+
+            thumbnail = (ImageView) itemView.findViewById(R.id.thumbnail);
+            selected = (LinearLayout) itemView.findViewById(R.id.selected);
+            progressBar = (ProgressBar) itemView.findViewById(R.id.progress);
+        }
+    }
+
+    public static class LoadThumbnail extends AsyncTask<Object, Void, Bitmap> {
+
+        String[] mVideos;
+        int position;
+        DataViewHolder dataViewHolder;
+
+        public LoadThumbnail(String[] mVideos, int position, RecyclerView.ViewHolder viewHolder) {
+            this.mVideos = mVideos;
+            this.position = position;
+            this.dataViewHolder = (DataViewHolder) viewHolder;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            dataViewHolder.progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected Bitmap doInBackground(Object... params) {
+            Bitmap thumbnail = ThumbnailUtils.createVideoThumbnail(mVideos[position],
+                    MediaStore.Images.Thumbnails.MINI_KIND);
+
+            return thumbnail;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap thumbnail) {
+            dataViewHolder.progressBar.setVisibility(View.GONE);
+            dataViewHolder.thumbnail.setImageBitmap(thumbnail);
+        }
+    }
 }
